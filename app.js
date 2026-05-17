a5545258800f6a998ad9a18c68f0b97e81935de6
Neeraj <Kambleneeraj74@gmail.com>
2026-05-17 13:36:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Jenkins Hello</h1>
+        <h2>Welcome</h2>
+    `);
+});
+
+app.listen(4000, () => {
+    console.log('Server running on port 4000');
+});
